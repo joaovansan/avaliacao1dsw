97814213c53dec2a90d7bf7d62736c00a25a1589
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-25 15:43:45 +0000

    Fix Banner correto

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,18 +1,19 @@
 export default function Banner() {
     return (
-<div className="flex h-[448px] items-center justify-center pl-[194.88px] pr-[194.89px] py-[128px] relative">
-<div className="inline-flex items-start gap-[20px] pt-[80px] pb-0 px-0 relative flex-[0_0_auto]">
-  <div className="items-start pt-[16px] pb-0 px-0 inline-flex flex-col relative flex-[0_0_auto]">
-    <div className="items-center pl-[0.21px] pr-[0.01px] py-0 inline-flex flex-col relative flex-[0_0_auto]">
-      <img
-        className="relative flex-[0_0_auto] mb-[-2.24px]"
-        alt="E commerce"
-        src="/workspaces/avaliacao1dsw/public/img/image.jpg"
-      />
-    </div>
-  </div>
-  <img className="relative self-stretch max-w-[634.23px] w-[100px]" alt="Image" src="image.png" />
-</div>
-</div>
-    )
-}
+      
+          <div className="inline-flex items-start gap-[20px] pt-[80px] pb-0 px-0 relative">
+            <div className="items-start pt-[16px] pb-0 px-0 inline-flex flex-col relative flex-[0_0_auto]">
+              <div className="items-center pl-[0.21px] pr-[0.01px] py-0 inline-flex flex-col relative flex-[0_0_auto]">
+                <img
+                  className="relative flex-[0_0_auto] mb-[-2.24px]"
+                  alt="E commerce"
+                  src="/workspaces/avaliacao1dsw/public/img/image.jpg"
+                />
+              </div>
+            </div>
+            <img className="relative self-stretch max-w-[634.23px] w-[100px]" alt="Image" src="image.jpg" />
+          </div>
+        );
+      };
+      
+    
