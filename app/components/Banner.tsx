c0137feb6b19f4548b05b7394c043c3eca6542c7
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-25 16:01:18 +0000

    Fix banner posição

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,9 +1,9 @@
 export default function Banner() {
     return (
                 
-      <div className="w-[514px] h-[96px]">
+      <div className="w-[514px] h-[96px] items-center">
       <img
-        className=" w-[509px] h-[94px] items-center"
+        className=" w-[509px] h-[94px]"
         alt="E commerces"
         src="/img/e-commerce-powerhouse-made-by-UTFPR.svg"
       />
