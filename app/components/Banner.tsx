ff881f5b08d9e4ed0929e821e0bc6430922a4c45
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-25 14:19:24 +0000

    Fix - Header + Banner

---
+++ app/components/Banner.tsx
@@ -0,0 +1,18 @@
+export default function Banner() {
+    return (
+<div className="flex h-[448px] items-center justify-center pl-[194.88px] pr-[194.89px] py-[128px] relative">
+<div className="inline-flex items-start gap-[20px] pt-[80px] pb-0 px-0 relative flex-[0_0_auto]">
+  <div className="items-start pt-[16px] pb-0 px-0 inline-flex flex-col relative flex-[0_0_auto]">
+    <div className="items-center pl-[0.21px] pr-[0.01px] py-0 inline-flex flex-col relative flex-[0_0_auto]">
+      <img
+        className="relative flex-[0_0_auto] mb-[-2.24px]"
+        alt="E commerce"
+        src="e-commerce-powerhouse-made-by-UTFPR.svg"
+      />
+    </div>
+  </div>
+  <img className="relative self-stretch max-w-[634.23px] w-[100px]" alt="Image" src="image.png" />
+</div>
+</div>
+    )
+}
