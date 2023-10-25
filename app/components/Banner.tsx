54a18f26308e89c79a0ee1715d6c706552e9801d
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-25 15:57:39 +0000

    Fix Banner items

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -3,7 +3,7 @@ export default function Banner() {
                 
       <div className="w-[514px] h-[96px]">
       <img
-        className=" w-[509px] h-[94px] top-0 left-0 items-center"
+        className=" w-[509px] h-[94px] items-center"
         alt="E commerces"
         src="/img/e-commerce-powerhouse-made-by-UTFPR.svg"
       />
