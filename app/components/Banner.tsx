701b1320d20439c617e3de9fc102a9f8f82c1186
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-25 16:40:36 +0000

    Fix banner image + texto

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,15 +1,20 @@
 export default function Banner() {
     return (
-                
-      <div className="w-[514px] h-[96px] items-center">
-      <img
-        className=" w-[509px] h-[94px]"
-        alt="E commerces"
-        src="/img/e-commerce-powerhouse-made-by-UTFPR.svg"
-      />
-    </div>
-    
-        );
+      <div className="inline-flex items-start gap-[20px] pt-[80px] pb-0 px-0 relative">
+      <div className="items-start pt-[16px] pb-0 px-0 inline-flex flex-col relative flex-[0_0_auto]">
+        <div className="items-center pl-[0.21px] pr-[0.01px] py-0 inline-flex flex-col relative flex-[0_0_auto]">
+          <img
+            className="relative flex-[0_0_auto] mb-[-2.24px]"
+            alt="E commerce"
+            src="/img/e-commerce-powerhouse-made-by-UTFPR.svg"
+          />
+        </div>
+      </div>
+      <img className="relative self-stretch max-w-[634.23px] w-[100px]" alt="Image" src="image.jpg" />
+    </div>    
+      
+    )
+     
       };
       
     
