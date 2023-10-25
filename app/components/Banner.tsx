984bc773d211490f168b7554af93a045d52ecdc1
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-25 15:51:34 +0000

    Fix Banner 11

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,18 +1,14 @@
 export default function Banner() {
     return (
-      
-          <div className="inline-flex items-start gap-[20px] pt-[80px] pb-0 px-0 relative">
-            <div className="items-start pt-[16px] pb-0 px-0 inline-flex flex-col relative flex-[0_0_auto]">
-              <div className="items-center pl-[0.21px] pr-[0.01px] py-0 inline-flex flex-col relative flex-[0_0_auto]">
-                <img
-                  className="relative flex-[0_0_auto] mb-[-2.24px]"
-                  alt="E commerce"
-                  src="/workspaces/avaliacao1dsw/public/img/image.jpg"
-                />
-              </div>
-            </div>
-            <img className="relative self-stretch max-w-[634.23px] w-[100px]" alt="Image" src="image.jpg" />
-          </div>
+                
+      <div className="w-[514px] h-[96px]">
+      <img
+        className="fixed w-[509px] h-[94px] top-0 left-0"
+        alt="E commerces"
+        src="/img/e-commerce-powerhouse-made-by-UTFPR.svg"
+      />
+    </div>
+    
         );
       };
       
