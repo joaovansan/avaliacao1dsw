e19aa98795c8c67638187bf3c46b22eba252a516
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-26 12:24:51 +0000

    Fix ipad pro anuncio

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -17,7 +17,7 @@ export default function Main() {
 
 
 
-      <div className="flex flex-col w-[294px] items-start gap-[8px] pt-[13px] pb-[253px] px-[12px] absolute top-[243px] left-px bg-white border-t [border-top-style:solid] border-zinc-800">
+      <div className="flex flex-col w-[480px] items-start gap-[8px] pt-[13px] pb-[253px] px-[12px] absolute top-[243px] left-px bg-white border-t [border-top-style:solid] border-zinc-800">
         <div className="flex items-center justify-between relative self-stretch w-full flex-[0_0_auto]">
           <div className="inline-flex items-start pl-0 pr-[5.92px] py-0 flex-[0_0_auto] flex-col relative">
             <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[18px] tracking-[0] leading-[28px] whitespace-nowrap">
