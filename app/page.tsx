4c38f5d9cd307ecf62183f67b7c041f3fa8e5e1c
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-26 14:24:59 +0000

    Fix footer 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Banner from "./components/Banner"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Main from "./components/Main"
-import Footer from "./components/Footer"
 
 export default function Home() {
   return (
