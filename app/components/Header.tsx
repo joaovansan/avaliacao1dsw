e872a876b601d9786116ec1a5aae54da18d1545e
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-26 14:30:21 +0000

    Fix header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,48 +1,51 @@
 export default function Header() {
     return (
    
-      <div className="flex items-start flex pt-0 pb-[16px] px-[112px] relative bg-white border-b [border-bottom-style:solid] border-[#4040404c]">
-        <div className="h-[30px] w-[1440px] items-center justify-between pl-[10px] pr-[12.02px] pt-[20px] pb-0 relative mr-[-112.00px]">
-          <div className="inline-flex mt-[-9.00px] mb-[-9.00px] flex-col items-start relative flex-[0_0_auto]">
-            <div className="flex flex-col items-start pl-0 pr-[2.23px] py-0 relative self-stretch w-full flex-[0_0_auto]">
-              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[20px] tracking-[0] leading-[28px] whitespace-nowrap">
-                UTFPR
+      <div className="flex flex-col w-[1440px] h-[64px] items-start pt-0 pb-[16px] px-[112px] relative bg-white border-b [border-bottom-style:solid] border-[#4040404c]">
+      <div className="flex w-[912px] h-[30px] items-center justify-between pl-[10px] pr-[12.02px] pt-[20px] pb-0 relative mr-[-112.00px]">
+        <div className="inline-flex mt-[-9.00px] mb-[-9.00px] flex-col items-start relative flex-[0_0_auto]">
+          <div className="flex flex-col items-start pl-0 pr-[2.23px] py-0 relative self-stretch w-full flex-[0_0_auto]">
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[20px] tracking-[0] leading-[28px] whitespace-nowrap">
+              UTFPR
+            </div>
+          </div>
+        </div>
+        <div className="inline-flex items-start gap-[4px] relative flex-[0_0_auto] mt-[-5.00px] mb-[-5.00px]">
+          <div className="inline-flex flex-col items-start pl-[16px] pr-[14.48px] py-0 relative self-stretch flex-[0_0_auto]">
+            <div className="absolute w-[89px] h-[32px] top-[-5px] left-0 bg-[#181818] rounded-[8px] border border-solid border-[#40404066]" />
+            <div className="flex self-stretch w-full flex-col items-start relative flex-[0_0_auto]">
+              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-neutral-400 text-[14px] tracking-[0] leading-[20px] whitespace-nowrap">
+                Discover
               </div>
             </div>
           </div>
-          <div className="inline-flex items-start gap-[4px] relative flex-[0_0_auto] mt-[-5.00px] mb-[-5.00px]">
-            <div className="inline-flex flex-col items-start pl-[16px] pr-[14.48px] py-0 relative self-stretch flex-[0_0_auto]">
-              <div className="absolute w-[89px] h-[32px] top-[-5px] left-0 bg-[#181818] rounded-[8px] border border-solid border-[#40404066]" />
-              <div className="flex self-stretch w-full flex-col items-start relative flex-[0_0_auto]">
-                <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-neutral-400 text-[14px] tracking-[0] leading-[20px] whitespace-nowrap">
-                  Discover
-                </div>
+          <div className="inline-flex flex-col items-start pl-[16px] pr-[14.44px] py-0 relative self-stretch flex-[0_0_auto]">
+            <div className="flex self-stretch w-full flex-col items-start relative flex-[0_0_auto]">
+              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-neutral-400 text-[14px] tracking-[0] leading-[20px] whitespace-nowrap">
+                Templates
               </div>
             </div>
-            <div className="inline-flex flex-col items-start pl-[16px] pr-[14.44px] py-0 relative self-stretch flex-[0_0_auto]">
-              <div className="flex self-stretch w-full flex-col items-start relative flex-[0_0_auto]">
-                <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-neutral-400 text-[14px] tracking-[0] leading-[20px] whitespace-nowrap">
-                  Templates
-                </div>
+          </div>
+          <div className="inline-flex flex-col items-start pl-[16px] pr-[14.22px] py-0 relative self-stretch flex-[0_0_auto]">
+            <div className="flex self-stretch w-full flex-col items-start relative flex-[0_0_auto]">
+              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-neutral-400 text-[14px] tracking-[0] leading-[20px] whitespace-nowrap">
+                Mockups
               </div>
             </div>
-            <div className="inline-flex flex-col items-start pl-[16px] pr-[14.22px] py-0 relative self-stretch flex-[0_0_auto]">
-              <div className="flex self-stretch w-full flex-col items-start relative flex-[0_0_auto]">
-                <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-neutral-400 text-[14px] tracking-[0] leading-[20px] whitespace-nowrap">
-                  Mockups
-                </div>
-              </div>
-            </div>
-            <div className="inline-flex flex-col items-start pl-[16px] pr-[14.77px] py-0 relative self-stretch flex-[0_0_auto]">
-              <div className="flex self-stretch w-full flex-col items-start relative flex-[0_0_auto]">
-                <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-neutral-400 text-[14px] tracking-[0] leading-[20px] whitespace-nowrap">
-                  Graphics
-                </div>
+          </div>
+          <div className="inline-flex flex-col items-start pl-[16px] pr-[14.77px] py-0 relative self-stretch flex-[0_0_auto]">
+            <div className="flex self-stretch w-full flex-col items-start relative flex-[0_0_auto]">
+              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-neutral-400 text-[14px] tracking-[0] leading-[20px] whitespace-nowrap">
+                Graphics
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+}
+
+    
+  
   
