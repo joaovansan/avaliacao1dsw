e85644ac1022fd5a027f2a9529de6d26b185599e
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-27 20:14:42 +0000

    Fix footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,13 @@
 export default function Footer() {
     return (
-<div className="flex-col bg-white w-[1440px] gap-[32px] pt-[88px] pb-[32px] px-[32px] text-black flex items-start relative flex-[0_0_auto]">
-  <div className="md:flex  flex-col items-start gap-[40px] px-[112px] py-0 relative self-stretch w-full flex-[0_0_auto]">
-    <div className="relative w-[1440px] h-[2px] border border-solid border-[#40404040]" />
-    <div className="pr-[1.53e-05px] pl-0 py-0 relative self-stretch w-full flex-[0_0_auto] flex items-start">
-      <div className="md:flex  w-[147.19px] items-start pl-0 pr-[49.19px] pt-0 pb-[104px] flex-col relative">
-        <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[20px] tracking-[0] leading-[28px] whitespace-nowrap">
-          UTFPR
+<div className="bg-white p-8 md:p-0 text-black">
+      <div className="md:flex flex-col items-start gap-4 px-8 md:px-0 self-stretch w-full">
+        <div className="border border-solid border-[#40404040] w-full h-2" />
+        <div className="relative w-full">
+          <div className="font-normal text-2xl tracking-0 leading-7">
+            UTFPR
+          </div>
         </div>
-      </div>
       <div className="md:flex  flex-col w-[147.2px] items-start gap-[12px] relative self-stretch">
         <div className="md:flex  items-start pl-0 pr-[79.2px] py-0 self-stretch w-full flex-[0_0_auto] flex-col relative">
           <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
@@ -101,21 +100,18 @@ export default function Footer() {
         </div>
       </div>
     </div>
-    <div className="justify-between pt-[21px] pb-0 px-0 self-stretch w-full border-t [border-top-style:solid] border-zinc-700 flex items-start relative flex-[0_0_auto]">
-      <div className="inline-flex flex-col items-start pl-0 pr-[1.36px] py-0 relative self-stretch flex-[0_0_auto]">
-        <div className="relative w-fit mt-[-1.00px] [font-family:'Roboto-Regular',Helvetica] font-normal text-zinc-500 text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
+    <div className="pt-4 flex justify-between border-t border-zinc-700 items-start relative">
+        <div className="text-zinc-500 text-2xl font-normal">
           © UTFPR 2023
         </div>
+        <div className="flex items-start justify-center gap-4">
+          <img className="cursor-pointer" alt="Div cursor pointer" src="/img/SVG.svg" />
+          <div className="inline-flex flex-col items-start pt-0 pb-2 relative">
+            <img className="w-20 h-20" alt="Svg" src="/img/SVG (1).svg" />
+          </div>
+          <img className="cursor-pointer" alt="Div cursor pointer" src="/img/SVG (2).svg" />
+        </div>
       </div>
-      </div>
-        <div className="inline-flex items-start justify-center gap-[24px] relative">
-      <img className="relative flex-[0_0_auto]" alt="Div cursor pointer" src="/img/SVG.svg" />
-      <div className="inline-flex flex-col items-start pt-0 pb-[4px] px-0 relative flex-[0_0_auto]">
-        <img className="relative w-[20px] h-[20px]" alt="Svg" src="/img/SVG (1).svg" />
-      </div>
-      <img className="relative flex-[0_0_auto]" alt="Div cursor pointer" src="/img/SVG (2).svg" />
     </div>
-  </div>
-</div>
-)
+  );
 }
